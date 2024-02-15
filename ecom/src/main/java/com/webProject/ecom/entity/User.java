f8f7a993c65e7fdb6b82e802b10b94a456523eee
tanhnosleep <toilatanh7@gmail.com>
2024-02-15 17:02:48 +0700

    first commit

---
+++ ecom/src/main/java/com/webProject/ecom/entity/User.java
@@ -0,0 +1,27 @@
+package com.webProject.ecom.entity;
+
+import com.webProject.ecom.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
+}
---
+++ ecom/src/main/java/com/webProject/ecom/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.webProject.ecom.enums;
+
+public enum UserRole {
+    ADMIN,
+
+    CUSTOMER
+}
