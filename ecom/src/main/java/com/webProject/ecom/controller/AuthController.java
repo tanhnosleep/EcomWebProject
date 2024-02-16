5fde28a5a05ce0a141c303962d18ff475cf15339
Tom Black <blacktom201@gmail.com>
2024-02-16 15:58:44 +0700

    second commit

---
+++ ecom/src/main/java/com/webProject/ecom/controller/AuthController.java
@@ -0,0 +1,63 @@
+package com.webProject.ecom.controller;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.webProject.ecom.dto.AuthenticationRequest;
+import com.webProject.ecom.entity.User;
+import com.webProject.ecom.repository.UserRepository;
+import com.webProject.ecom.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final UserDetailsService userDetailsService;
+
+    private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    @PostMapping("/authenticate")
+    public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest,
+                                          HttpServletResponse response) throws IOException, JSONException {
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e){
+            throw new BadCredentialsException("Incorrect username or password");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        if(optionalUser.isPresent()){
+            response.getWriter().write(new JSONObject()
+                    .put("userId", optionalUser.get().getId())
+                    .put("role", optionalUser.get().getRole())
+                    .toString()
+            );
+
+            response.addHeader(HEADER_STRING,TOKEN_PREFIX + jwt);
+        }
+    }
+}
